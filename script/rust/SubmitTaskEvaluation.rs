6ea9f25f0d36800e37c8cddcdb393d4628bf28fa
David He <david.he@magic.link>
2025-11-04 10:35:21 -0500

    open sample intent, parse it and output it out console

--- script/rust/SubmitTaskEvaluation.rs
+++ script/rust/SubmitTaskEvaluation.rs
@@ -1,7 +1,14 @@
+use std::fs;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let response = reqwest::get("https://httpbin.org/get").await?;
     let body = response.text().await?;
     println!("Response: {}", body);
+
+    let contents = fs::read_to_string("../../sample_intent.json")?;
+    let json: serde_json::Value = serde_json::from_str(&contents)?;    
+    println!("{}", serde_json::to_string_pretty(&json)?);
+        
     Ok(())
 }
